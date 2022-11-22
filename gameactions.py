6fc20758df62236a6fef995ed3f7ee1b5ac9f522
Vincent Liu <Crystalmew223@gmail.com>
2022-11-22 12:17:42 -0500

    Created gameactions.py
    a helper file for making game actions

---
+++ gameactions.py
@@ -0,0 +1,42 @@
+import pyautogui
+
+def level_up(ability):
+  keyboard.press_and_release('ctrl+' + ability)
+  
+  
+#Use Q
+def use_q(direction):
+  
+#Use W 
+def attach_to_champ():
+  
+#Use E  
+def heal_champ():
+  
+#Use R  
+def use_ult(direction):
+  
+#Buy Items
+def go_shopping():
+  
+#Recall
+def recall():
+  
+#Use Ignite
+def use_ignite():
+  
+#Use Exhaust
+def use_exhaust():
+  
+#Use Heal (Summoner)
+def use_heal_sum():
+  
+#Use a stealth ward
+def place_stealthward():
+  
+#Use pinkward
+def place_pinkward():
+  
+#Use Redmeption
+def use_redemption():
+  
