df5e64cf270aa006228baa25eb08e8291ea8cb9e
Vincent Liu <Crystalmew223@gmail.com>
2022-11-27 10:24:18 -0500

    Update gameactions.py

--- gameactions.py
+++ gameactions.py
@@ -1,18 +1,20 @@
 import pyautogui
 
+#Level up an ability
 def level_up(ability):
   keyboard.press_and_release('ctrl+' + ability)
-  
   
 #Use Q
 def use_q(direction):
   
 #Use W 
 def attach_to_champ():
-  
+  keyboard.press('W')
+
 #Use E  
 def heal_champ():
-  
+  keyboard.press('E')
+
 #Use R  
 def use_ult(direction):
   
@@ -21,13 +23,16 @@ def go_shopping():
   
 #Recall
 def recall():
-  
+  keyboard.press('B')
+
 #Use Ignite
 def use_ignite():
-  
+  keyboard.press('D')
+
 #Use Exhaust
 def use_exhaust():
-  
+  keyboard.press('F')
+
 #Use Heal (Summoner)
 def use_heal_sum():
   
