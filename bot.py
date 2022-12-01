2d4d0e8fb9493a60194625fd1542c4eb5fb79ba4
Vincent Liu <Crystalmew223@gmail.com>
2022-11-30 20:17:37 -0500

    Tweaked W and added more quotes

--- bot.py
+++ bot.py
@@ -11,8 +11,14 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 command_q = []
-command_w = ['Entr0py: Press W.', 'Entr0py: Attach to me.', 'Entr0py: Follow me.', 'Entr0py: Stop following.', 'Entr0py: Attach.', 'Entr0py: Detach.']
-command_e = ['Entr0py: Heal me.', 'Entr0py: Save me.', 'Entr0py: Press.', 'Entr0py: Press e.']
+command_w_attach = ['Entr0py: Press W.', 'Entr0py: Attach to me.', 'Entr0py: Follow me.', 'Entr0py: Attach.']
+command_w_detach = ['Entr0py: Press W.', 'Entr0py: Stop following.', 'Entr0py: Detach.']
+command_e = ['Entr0py: Heal me.', 'Entr0py: Save me.', 'Entr0py: Press.', 'Entr0py: Press e.', 'Entr0py: Kill me.']
+command_recall = ['Entr0py: Press B.', 'Entr0py: Back.', 'Entr0py: Base.', 'Entr0py: Space.', 'Entr0py: Bas.', 'Entr0py: Recall.']
+command_ward = ['Entr0py: Ward here.', 'Entr0py: Toward here.', 'Entr0py: Board here.', 'Entr0py: Word here.']
+command_toxic = ['Entr0py: Be toxic.', 'Entr0py: Detoxic.']
+command_chitchat = ['Entr0py: Chit chat.', 'Entr0py: Say something.', 'Entr0py: Something?']
+debug_center = ['Entr0py: Center mouse.']
 
 @client.event
 async def on_ready():
@@ -23,19 +29,44 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content in command_w:
-        response = 'Pressing W'
+    if message.content in command_w_attach:
+        use_w_attach()
+        response = 'Attaching'
+        await message.channel.send(response)        
+    
+    if message.content in command_w_detach:
+        use_w_detach()
+        response = 'Detaching'
         await message.channel.send(response)
-        use_w()
     
     if message.content in command_e:
-        response = 'Pressing E'
-        await message.channel.send(response)
         use_e()
+        response = 'Pressing E'
+        await message.channel.send(response)        
+    
+    if message.content in command_recall:
+        recall()
+        response = 'Recalling'
+        await message.channel.send(response)
         
-    if message.content == 'Entr0py: Be toxic.':
+    if message.content in command_ward:
+        place_stealthward()
+        response = 'Placing Stealth Ward'
+        await message.channel.send(response)    
+        
+    if message.content in command_toxic:
+        being_toxic()
         response = 'Being Toxic'
         await message.channel.send(response) 
-        being_toxic()
+
+    if message.content in command_chitchat:
+        chitchat()
+        response = 'Chit chatting'
+        await message.channel.send(response) 
+
+    if message.content in debug_center:
+        center_mouse()
+        response = 'Centering mouse'
+        await message.channel.send(response)
         
 client.run(TOKEN)
--- gameactions.py
+++ gameactions.py
@@ -1,128 +1,139 @@
-import pyautogui as keyboard
+import pyautogui
+from keystrokes import *
 import time
-import ctypes
 import random
 
-R = 0x13
-E = 0x12
-W = 0x11
-Q = 0x10
-Enter = 0x1C
-
-# Bunch of stuff so that the script can send keystrokes to game #
-
-SendInput = ctypes.windll.user32.SendInput
-
-# C struct redefinitions 
-PUL = ctypes.POINTER(ctypes.c_ulong)
-class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort),
-                ("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class HardwareInput(ctypes.Structure):
-    _fields_ = [("uMsg", ctypes.c_ulong),
-                ("wParamL", ctypes.c_short),
-                ("wParamH", ctypes.c_ushort)]
-
-class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long),
-                ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong),
-                ("dwFlags", ctypes.c_ulong),
-                ("time",ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class Input_I(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput),
-                 ("mi", MouseInput),
-                 ("hi", HardwareInput)]
-
-class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong),
-                ("ii", Input_I)]
-
-# Actuals Functions
-
-def PressKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-def ReleaseKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-def KeyPress(key):
-    PressKey(key) # press 
-    time.sleep(.05)
-    ReleaseKey(key) #release 
-
+KEYS = {'R':0x13,
+        'E':0x12,
+        'W':0x11,
+        'Q':0x10,
+        'B':0x30,
+        'D':0x20,
+        'F':0x21,
+        'Enter':0x1C,
+        'Ctrl':0x1D,
+        '4':0x05}
 
 #Level up an ability
 def level_up(ability):
-  keyboard.press_and_release('ctrl+' + ability)
+  PressKey(KEYS['Ctrl'])
+  time.sleep(.01)
+  KeyPress(KEYS[ability])
+  time.sleep(.01)
+  ReleaseKey(KEYS['Ctrl'])
+  time.sleep(.01)  
+  
   
 #Use Q
 def use_q(direction):
   return 0
   
-#Use W 
-def use_w():
-  KeyPress(W)
+  
+#Use W (Attaching)
+def use_w_attach():
+  center_mouse()
+  k = 200
+  
+  KeyPress(KEYS['W'])
+  pyautogui.move(k, k)  #move to 9
+  KeyPress(KEYS['W'])
+  pyautogui.move(0, -k) #move to 6
+  KeyPress(KEYS['W'])
+  pyautogui.move(0, -k) #move to 3
+  KeyPress(KEYS['W'])
+  pyautogui.move(-k, 0) #move to 2
+  KeyPress(KEYS['W'])
+  pyautogui.move(-k, 0) #move to 1
+  KeyPress(KEYS['W'])
+  pyautogui.move(0, k)  #move to 4
+  KeyPress(KEYS['W'])
+  pyautogui.move(0, k)  #move to 7
+  KeyPress(KEYS['W'])
+  pyautogui.move(k, 0)  #move to 8
+  KeyPress(KEYS['W'])
+  
+  center_mouse()
+
+
+#Use W (Detaching)
+def use_w_detach():
+  center_mouse()
+  KeyPress(KEYS['W'])
+
 
 #Use E  
 def use_e():
-  KeyPress(E)
+  KeyPress(KEYS['E'])
+
 
 #Use R  
 def use_ult(direction):
-  return 0
+  KeyPress(KEYS['R'])
+  
   
 #Buy Items
 def go_shopping():
   return 0
   
+  
 #Recall
 def recall():
-  keyboard.press('B')
+  KeyPress(KEYS['B'])
+
 
 #Use Ignite
 def use_ignite():
-  keyboard.press('D')
+  KeyPress(KEYS['D'])
+
 
 #Use Exhaust
 def use_exhaust():
-  keyboard.press('F')
+  KeyPress(KEYS['F'])
+
 
 #Use Heal (Summoner)
 def use_heal_sum():
-  keyboard.press('D')
+  KeyPress(KEYS['D'])
+  
   
 #Use a stealth ward
 def place_stealthward():
-  return 0
+  KeyPress(KEYS['4'])
+  
   
 #Use pinkward
 def place_pinkward():
   return 0
   
+  
 #Use Redmeption
 def use_redemption():
   return 0
   
+  
 #Be Toxic
-toxic_phrases = ['GGEZ', 'Git gud', 'Sup diff', 'GG uninstall', 'EZ uninstall']
+toxic_phrases = ['GGEZ', 'Git gud', 'sup diff', 'GG uninstall', 'EZ uninstall', 'Noobs', 'ggez', 'sup gap', 'jg diff', 'ff15']
+toxic_len = len(toxic_phrases) - 1
+
 def being_toxic():
-  KeyPress(Enter)
+  KeyPress(KEYS['Enter'])
   time.sleep(0.01)
-  keyboard.write(toxic_phrases[random.randint(0,4)], interval = 0.01)
+  pyautogui.write(toxic_phrases[random.randint(0,toxic_len)])
   time.sleep(0.01)
-  KeyPress(Enter)
+  KeyPress(KEYS['Enter'])
+  
+  
+#Chit-chat
+quotes = ["Keep an eye out for red dot, Book! It's gotta be here somewhere.", "Where to, Book? Purr~", "Book, float us that-a-way!", "I hiss at you, giant pile of stones! Hiss!", "Cat-tack!", "We got the zoomies!", "OwO wats dis~?", "You look like unscratched furniture!", "I'm Yuumi, and you're warm!", "Beep boop! Hi I'm Yuumi bot!", ]
+quotes_len = len(quotes) - 1
+
+def chitchat():
+  KeyPress(KEYS['Enter'])
+  time.sleep(0.01)
+  pyautogui.write(quotes[random.randint(0,quotes_len)])
+  time.sleep(0.01)
+  KeyPress(KEYS['Enter'])
+  
+  
+def center_mouse():
+  pyautogui.moveTo(960, 500)
