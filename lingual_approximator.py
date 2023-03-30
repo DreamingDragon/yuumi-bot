1d12c5b74064c07d8d13cf96d1db269ea3de8d9d
Vincent Liu <Crystalmew223@gmail.com>
2023-03-30 11:04:49 -0400

    new code for Lingual aprox
    is still faulty

--- lingual_approximator.py
+++ lingual_approximator.py
@@ -1,6 +1,28 @@
+import numpy as np
+
+vecFile = open("cmudict-0.7b-simvecs", "r")
+fileLines = vecFile.readlines()
+vectorDict = {}
+    
+for line in fileLines:
+    elements = line.split(" ")
+    lable = elements[0]
+    vectorDict[lable] = np.array([])
+    elements.remove(lable)
+    elements.remove("")
+    for vecValue in elements:
+        vectorDict[lable] = np.append(vectorDict[lable], float(vecValue))
+
 def phonetic_approximant(recievedString):
-    vecFile = open(r"cmudict-0.7b-simvecs", "Access_Mode")
+    recievedString = recievedString.upper()
+    splitString = recievedString.split(" ")
+    commandVector = np.zeros(50)
+    
+    for word in splitString:
+        wordVector = vectorDict[word]
+        commandVector = np.add(commandVector, wordVector)
+        
+    cos_sim = dot(commandVector, wordVector)/(norm(commandVector)*norm(wordVector))
     
 #Debug
-vecFile = open(r"cmudict-0.7b-simvecs", "r")
-print(vecFile.read(1000))
+#in the format: Word, 50 vector values
