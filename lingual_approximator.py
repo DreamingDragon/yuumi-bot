e5bd8347cceb0decd455a674c505359f97e84c34
Vincent Liu <Crystalmew223@gmail.com>
2023-03-30 12:37:53 -0400

    Update lingual_approximator.py
    The cosine distances works. Need to populate commands and wordVectors

--- lingual_approximator.py
+++ lingual_approximator.py
@@ -1,8 +1,14 @@
 import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy import spatial
 
 vecFile = open("cmudict-0.7b-simvecs", "r")
 fileLines = vecFile.readlines()
+global vectorDict 
 vectorDict = {}
+
+global commandDict
+commandDict = {}
     
 for line in fileLines:
     elements = line.split(" ")
@@ -12,17 +18,35 @@ for line in fileLines:
     elements.remove("")
     for vecValue in elements:
         vectorDict[lable] = np.append(vectorDict[lable], float(vecValue))
+        
+#debug
+commandDict["APPLE"] = vectorDict["APPLE"]
+commandDict["SILENCE"] = vectorDict["SILENCE"]
+
 
 def phonetic_approximant(recievedString):
     recievedString = recievedString.upper()
     splitString = recievedString.split(" ")
-    commandVector = np.zeros(50)
+    recievedVector = np.zeros(50)
+    smallest_cos_sim = 1000
+    matchingCommand = ""
     
     for word in splitString:
         wordVector = vectorDict[word]
-        commandVector = np.add(commandVector, wordVector)
-        
-    cos_sim = dot(commandVector, wordVector)/(norm(commandVector)*norm(wordVector))
+        recievedVector = np.add(recievedVector, wordVector)
+    
+    for key in commandDict:
+        commandVector = commandDict[key]
+        cos_sim = spatial.distance.cosine(recievedVector, commandVector)
+        if cos_sim < smallest_cos_sim:
+            smallest_cos_sim = cos_sim
+            matchingCommand = key
+            
+    return (matchingCommand, smallest_cos_sim)
+    
+    
     
 #Debug
 #in the format: Word, 50 vector values
+print(commandDict["SILENCE"])
+print(phonetic_approximant("DIAMONDS"))
